86eaaa6d4b44704f32e10419bbcfd000a9b65b03
gavi-dorf <gavidorfman@gmail.com>
2026-02-25 16:00:38 +0200

    Initial commit

---
+++ lib/appwork_take_home.ex
@@ -0,0 +1,18 @@
+defmodule AppworkTakeHome do
+  @moduledoc """
+  Documentation for `AppworkTakeHome`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AppworkTakeHome.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/appwork_take_home_test.exs
@@ -0,0 +1,8 @@
+defmodule AppworkTakeHomeTest do
+  use ExUnit.Case
+  doctest AppworkTakeHome
+
+  test "greets the world" do
+    assert AppworkTakeHome.hello() == :world
+  end
+end
